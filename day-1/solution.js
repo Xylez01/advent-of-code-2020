9c204df007106c905be5bc34f73bd9bc96378abb
Jordy Langen <jordy.langen@signify.com>
2020-12-02 10:36:44 +0100

    Day 1, part 2

--- day-1/solution.js
+++ day-1/solution.js
@@ -1,22 +1,39 @@
 const fs = require('fs');
 
-function solvePartOne(numbers) {
-  const solution = numbers
-    .flatMap((number, index) =>
-      numbers.slice(index + 1).map(other => {
-        return {left: number, right: other, sum: number + other}
-      })
-    )
-    .find(combination => combination.sum === 2020);
+function combinations(numbers, number, index) {
+  return numbers.slice(index + 1).map(other => {
+    return {result: [number, other], sum: number + other}
+  })
+}
 
-  return solution.left * solution.right;
+function solvePartOne(numbers) {
+  return numbers
+    .flatMap((number, index) => combinations(numbers, number, index))
+    .find(combination => combination.sum === 2020)
+    .result.reduce((left, right) => left * right)
+}
+
+function solvePartTwo(numbers) {
+  return numbers
+    .flatMap((number, index) => {
+      const remainingNumbers = numbers.slice(index + 1);
+      return remainingNumbers
+        .flatMap((remainingNumber, indexOfRemainingNumber) =>
+          combinations(remainingNumbers, remainingNumber, indexOfRemainingNumber))
+        .map(combination => {
+          combination.result.push(number)
+          return {result: combination.result, sum: combination.sum + number}
+        })
+    })
+    .find(combination => combination.sum === 2020)
+    .result.reduce((left, right) => left * right)
 }
 
 module.exports = {
   solve() {
-    const fileContent = fs.readFileSync('./day-1/input.txt', 'utf8').trimEnd()
+    const fileContent = fs.readFileSync('./day-1/input.txt', 'utf8')
     const numbers = fileContent.split('\r\n').map(numberAsText => Number(numberAsText))
 
-    return {partOne: solvePartOne(numbers)}
+    return {partOne: solvePartOne(numbers), partTwo: solvePartTwo(numbers)}
   }
 }
