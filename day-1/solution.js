a860c8774b151fa8b9bab3081894d9b2942f5b78
Jordy Langen <jordy.langen@signify.com>
2020-12-02 10:56:24 +0100

    Added a formatter and the ability to run specific solutions from the cmd

--- day-1/solution.js
+++ day-1/solution.js
@@ -2,15 +2,15 @@ const fs = require('fs');
 
 function combinations(numbers, number, index) {
   return numbers.slice(index + 1).map(other => {
-    return {result: [number, other], sum: number + other}
-  })
+    return { result: [number, other], sum: number + other };
+  });
 }
 
 function solvePartOne(numbers) {
   return numbers
     .flatMap((number, index) => combinations(numbers, number, index))
     .find(combination => combination.sum === 2020)
-    .result.reduce((left, right) => left * right)
+    .result.reduce((left, right) => left * right);
 }
 
 function solvePartTwo(numbers) {
@@ -19,21 +19,22 @@ function solvePartTwo(numbers) {
       const remainingNumbers = numbers.slice(index + 1);
       return remainingNumbers
         .flatMap((remainingNumber, indexOfRemainingNumber) =>
-          combinations(remainingNumbers, remainingNumber, indexOfRemainingNumber))
+          combinations(remainingNumbers, remainingNumber, indexOfRemainingNumber)
+        )
         .map(combination => {
-          combination.result.push(number)
-          return {result: combination.result, sum: combination.sum + number}
-        })
+          combination.result.push(number);
+          return { result: combination.result, sum: combination.sum + number };
+        });
     })
     .find(combination => combination.sum === 2020)
-    .result.reduce((left, right) => left * right)
+    .result.reduce((left, right) => left * right);
 }
 
 module.exports = {
   solve() {
-    const fileContent = fs.readFileSync('./day-1/input.txt', 'utf8')
-    const numbers = fileContent.split('\r\n').map(numberAsText => Number(numberAsText))
+    const fileContent = fs.readFileSync('./day-1/input.txt', 'utf8');
+    const numbers = fileContent.split('\r\n').map(numberAsText => Number(numberAsText));
 
-    return {partOne: solvePartOne(numbers), partTwo: solvePartTwo(numbers)}
+    return { partOne: solvePartOne(numbers), partTwo: solvePartTwo(numbers) };
   }
-}
+};
--- index.js
+++ index.js
@@ -1,3 +1,24 @@
-const dayOne = require('./day-1/solution')
+const dayOne = require('./day-1/solution');
 
-console.log( {dayOne: dayOne.solve()})
+const arguments = process.argv.slice(2);
+
+const adventOfCode = {
+  dayOne: function () {
+    return dayOne.solve();
+  }
+};
+
+if (arguments.length === 0) {
+  const solutions = {};
+  Object.keys(adventOfCode).forEach(key => {
+    solutions[key] = adventOfCode[key]();
+  });
+
+  console.log(solutions);
+} else {
+  const solution = {};
+  const dayArgument = arguments[0];
+  solution[dayArgument] = adventOfCode[dayArgument]();
+
+  console.log(solution);
+}
