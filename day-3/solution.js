6a3f8cf13642976e051aa6460a50631024678dc7
Jordy Langen <jordy.langen@signify.com>
2020-12-04 09:01:49 +0100

    day 4 solution in dart :tada:

--- day-3/solution.js
+++ day-3/solution.js
@@ -21,26 +21,28 @@ const traverseMap = (grid, movesToRight, movesDownward) => {
   return treesHit;
 };
 
+const solve = () => {
+  const grid = fs.readFileSync('./day-3/input.txt', 'utf8').split('\r\n');
+  const depth = grid.length;
+  const width = grid[0].length;
+
+  const positionsToGoRight = depth * maxMovesToRight;
+  const patternRepeatCount = Math.ceil(positionsToGoRight / width);
+
+  const patternedGrid = grid.map(row => row.repeat(patternRepeatCount));
+
+  return {
+    partOne: traverseMap(patternedGrid, 3, 1),
+    partTwo: [
+      traverseMap(patternedGrid, 1, 1),
+      traverseMap(patternedGrid, 3, 1),
+      traverseMap(patternedGrid, 5, 1),
+      traverseMap(patternedGrid, 7, 1),
+      traverseMap(patternedGrid, 1, 2)
+    ].reduce((left, right) => left * right)
+  };
+}
+
 module.exports = {
-  solve() {
-    const grid = fs.readFileSync('./day-3/input.txt', 'utf8').split('\r\n');
-    const depth = grid.length;
-    const width = grid[0].length;
-
-    const positionsToGoRight = depth * maxMovesToRight;
-    const patternRepeatCount = Math.ceil(positionsToGoRight / width);
-
-    const patternedGrid = grid.map(row => row.repeat(patternRepeatCount));
-
-    return {
-      partOne: traverseMap(patternedGrid, 3, 1),
-      partTwo: [
-        traverseMap(patternedGrid, 1, 1),
-        traverseMap(patternedGrid, 3, 1),
-        traverseMap(patternedGrid, 5, 1),
-        traverseMap(patternedGrid, 7, 1),
-        traverseMap(patternedGrid, 1, 2)
-      ].reduce((left, right) => left * right)
-    };
-  }
+  solve
 };
---
+++ day-4/solution.dart
@@ -0,0 +1,76 @@
+import 'dart:io';
+
+Future<List<String>> readInput() async => (await new File('./day-4/input.txt').readAsString()).split('\r\n\r\n');
+
+bool hasLength(String input, int length) => input.length == length;
+bool isInRange(int value, int min, int max) => value >= min && value <= max;
+bool isLengthExpression(String input) => new RegExp(r'(\d*)(cm|in)').hasMatch(input);
+bool isHexColor(String input) => new RegExp(r'#([0-9a-z])').hasMatch(input);
+bool isNumber(String input) => new RegExp(r'(\d*)').hasMatch(input);
+
+bool isValidLength(String input) {
+  final unit = input.substring(input.length - 2);
+  final value = int.parse(input.substring(0, input.length - 2));
+
+  if (unit == 'cm') {
+    return isInRange(value, 150, 193);
+  } else {
+    return isInRange(value, 59, 76);
+  }
+}
+
+var requiredFields = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'];
+var validEyeColors = ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'];
+
+var fieldValidators = {
+  'byr': (String input) => hasLength(input, 4) && isInRange(int.parse(input), 1920, 2002),
+  'iyr': (String input) => hasLength(input, 4) && isInRange(int.parse(input), 2010, 2020),
+  'eyr': (String input) => hasLength(input, 4) && isInRange(int.parse(input), 2020, 2030),
+  'hcl': (String input) => hasLength(input, 7) && isHexColor(input),
+  'ecl': (String input) => hasLength(input, 3) && validEyeColors.any((color) => color == input),
+  'pid': (String input) => hasLength(input, 9) && isNumber(input),
+  'hgt': (String input) => isLengthExpression(input) && isValidLength(input)
+};
+
+class Passport {
+  Map<String, String> fields;
+
+  bool get hasRequiredFields {
+    return requiredFields.every((key) => fields.containsKey(key));
+  }
+
+  Passport(this.fields) {}
+
+  bool validate() {
+    if (!this.hasRequiredFields) {
+      return false;
+    }
+
+    return fieldValidators.keys.every((key) {
+      var validator = fieldValidators[key];
+      var input = this.fields[key] ?? '';
+      return validator?.call(input) ?? false;
+    });
+  }
+}
+
+void main() async {
+  final fileContent = await readInput();
+
+  var passports = fileContent.map((element) {
+    final fields = element.split('\r\n').join(' ').split(' ').map((value) {
+      var keyValue = value.split(':');
+      return new MapEntry(keyValue[0], keyValue[1]);
+    });
+
+    return new Passport(new Map.fromEntries(fields));
+  });
+
+  var passportsWithAllRequiredFields = passports.where((passport) => passport.hasRequiredFields).length;
+
+  print('Passports with all required fields: ${passportsWithAllRequiredFields}');
+
+  var validPassports = passports.where((passport) => passport.validate()).length;
+
+  print('Passports that are 100% valid: ${validPassports}');
+}
