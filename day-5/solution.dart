ce09dce3615b98e093aabca380b4a9a1f84337b4
Jordy Langen <jordy.langen@signify.com>
2020-12-05 12:52:52 +0100

    Solved day 5, part 1

--- day-5/solution.dart
+++ day-5/solution.dart
@@ -1,8 +1,18 @@
 import 'dart:io';
+import 'dart:math';
 
 Future<List<String>> readInput() async => (await new File('./day-5/input.txt').readAsString()).split('\r\n');
 
 List<String> characters(String input) => input.split('');
+Range lowerHalf(Range range) => Range(range.min, ((range.min + range.max) / 2).floor());
+Range upperHalf(Range range) => Range(((range.min + range.max) / 2).ceil(), range.max);
+
+final commands = {
+  'F': (Range range) => lowerHalf(range),
+  'B': (Range range) => upperHalf(range),
+  'L': (Range range) => lowerHalf(range),
+  'R': (Range range) => upperHalf(range)
+};
 
 class Range {
   int min;
@@ -12,21 +22,34 @@ class Range {
 }
 
 class BoardingPass {
+  int row;
+  int column;
 
-  BoardingPass.fromInput(String input) {
+  int get seatId => (row * 8) + column;
+
+  BoardingPass(this.row, this.column);
+
+  factory BoardingPass.fromInput(String input) {
     var rowCommands = input.substring(0, input.lastIndexOf('F'));
     var columnCommands = input.substring(input.lastIndexOf('F') + 1);
-    
-    characters(rowCommands).forEach((command) { 
-      print(command);
+
+    var rowRange = Range(0, 127);
+    characters(rowCommands).forEach((command) {
+      rowRange = commands[command]?.call(rowRange) ?? rowRange;
     });
 
-    print(columnCommands);
+    var columnRange = Range(0, 7);
+    characters(columnCommands).forEach((command) {
+      columnRange = commands[command]?.call(columnRange) ?? columnRange;
+    });
+
+    return BoardingPass(rowRange.min, columnRange.max);
   }
 }
 
 void main() async {
-  final fileContent = (await readInput()).map((input) => BoardingPass.fromInput(input));
+  final fileContent =
+      (await readInput()).map((input) => BoardingPass.fromInput(input)).map((pass) => pass.seatId).reduce(max);
 
   print(fileContent);
 }
